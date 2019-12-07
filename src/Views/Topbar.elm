8db49d14e529222fdb1095e2a7ae42aeb8649a62
BeardedBear <germain.poirrier@gmail.com>
2019-12-07 16:13:48 +0100

    Fix topbar centered elements.

--- src/Views/Topbar.elm
+++ src/Views/Topbar.elm
@@ -12,9 +12,9 @@ view : Session -> Html msg
 view _ =
     div [ class "Topbar" ]
         [ a [ Route.href Route.Home ] [ img [ class "Topbar__logo", src "./img/logo.svg" ] [] ]
-        , div [ class "TopBarNavigation" ]
-            [ button [ class "TopBarNavigation__item Button nude" ] [ i [ class "icon-previous" ] [] ]
-            , button [ class "TopBarNavigation__item Button nude disabled" ] [ i [ class "icon-next" ] [] ]
+        , div [ class "TopbarNavigation" ]
+            [ button [ class "Button nude" ] [ i [ class "icon-previous TopbarNavigation__icon " ] [] ]
+            , button [ class "Button nude disabled" ] [ i [ class "icon-next TopbarNavigation__icon " ] [] ]
             ]
         , Search.view
         , User.view
