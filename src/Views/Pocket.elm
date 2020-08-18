53c5f060d7e109e10bccab2f2a390a1e3ab3f74a
BeardedBear <germain.poirrier@gmail.com>
2020-08-18 09:19:38 +0200

    Add to collection.

--- src/Views/Pocket.elm
+++ src/Views/Pocket.elm
@@ -1,10 +1,12 @@
 module Views.Pocket exposing (Model, Msg, defaultModel, init, update, view)
 
+import Data.Id exposing (idToString)
 import Data.Pocket exposing (Pocket, defaultPocket)
 import Data.Session exposing (Session)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Html.Extra as HE
 import Http
 import Request.Playlist
 import Task
@@ -17,7 +19,7 @@ type alias Model =
 
 type Msg
     = Reset
-    | Add
+    | Add String
     | Played (Result ( Session, Http.Error ) ())
 
 
@@ -39,12 +41,11 @@ update ({ pocket } as session) msg model =
         Reset ->
             ( model, { session | pocket = { pocket | albums = [] } }, Cmd.none )
 
-        Add ->
-            let
-                _ =
-                    pocket.albums
-            in
-            ( model, session, Task.attempt Played (Request.Playlist.addAlbum session "3CL7dAZOAWOfJWLkO3YyTo" pocket.albums) )
+        Add playlistId ->
+            ( model
+            , { session | pocket = { pocket | albums = [] } }
+            , Task.attempt Played (Request.Playlist.addAlbum session playlistId pocket.albums)
+            )
 
         Played (Ok _) ->
             ( model, session, Cmd.none )
@@ -54,12 +55,21 @@ update ({ pocket } as session) msg model =
 
 
 view : Model -> Session -> Html Msg
-view _ session =
-    div [ class "Pocket" ]
-        [ div [ class "Pocket__head" ] [ button [ onClick Reset ] [ text "Reset" ] ]
-        , div []
-            (session.pocket.albums
-                |> List.map (\e -> div [] [ text e ])
-            )
-        , button [ onClick Add ] [ text "GO" ]
-        ]
+view model session =
+    HE.viewIf (model.pocket.albums == [])
+        (div [ class "Pocket" ]
+            [ div [ class "Pocket__head" ] [ button [ onClick Reset ] [ text "Reset" ] ]
+            , div []
+                (session.pocket.albums
+                    |> List.map (\e -> div [] [ text e ])
+                )
+            , div [ class "PocketPlaylists" ]
+                (session.playlists.items
+                    |> List.filter (\playlist -> String.startsWith "#Collection" playlist.name)
+                    |> List.map
+                        (\playlist ->
+                            div [ onClick <| Add (idToString playlist.id), class "PocketPlaylists__item" ] [ text playlist.name ]
+                        )
+                )
+            ]
+        )
